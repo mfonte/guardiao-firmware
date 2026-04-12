c9543d5d12747d6fc3c98f2880417b973aecc411
mfonte <mfonte@gmail.com>
2026-04-12 08:22:17 -0300

    feat(04-01): create native test env, Arduino/EEPROM stubs, and logic.h
    - Append [env:native] to platformio.ini with Unity test framework
    - Create test/stubs/Arduino.h with String class and SerialStub
    - Create test/stubs/EEPROM.h with RAM-backed EEPROMClass
    - Extract calculateStatusBitmap() and evaluateThreshold() to src/logic.h

---
+++ test/stubs/EEPROM.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <cstdint>
+#include <cstring>
+
+class EEPROMClass
+{
+public:
+  uint8_t _data[512];
+
+  EEPROMClass() { memset(_data, 0xFF, sizeof(_data)); }
+
+  void begin(int) {}
+  uint8_t read(int addr) { return _data[addr]; }
+  void write(int addr, uint8_t val) { _data[addr] = val; }
+
+  template <typename T>
+  void put(int addr, const T &val)
+  {
+    memcpy(&_data[addr], &val, sizeof(T));
+  }
+
+  template <typename T>
+  void get(int addr, T &val)
+  {
+    memcpy(&val, &_data[addr], sizeof(T));
+  }
+
+  void commit() {}
+  void clear() { memset(_data, 0xFF, sizeof(_data)); }
+};
+
+extern EEPROMClass EEPROM;
